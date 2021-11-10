34e7f4ecbf1e3183ecb40cc742393c5aeacc23f4
khusamov Albert <khusamov01@mail.ru>
2021-11-10 14:24:01 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace Method_of_calculation_4
+{
+    partial class Main_Form
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main_Form));
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Result_btn = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.BackgroundColor = System.Drawing.Color.Snow;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dataGridView.GridColor = System.Drawing.Color.LightGray;
+            this.dataGridView.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(1182, 479);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // Result_btn
+            // 
+            this.Result_btn.BackColor = System.Drawing.Color.Snow;
+            this.Result_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Result_btn.ForeColor = System.Drawing.Color.Crimson;
+            this.Result_btn.Location = new System.Drawing.Point(553, 498);
+            this.Result_btn.Name = "Result_btn";
+            this.Result_btn.Size = new System.Drawing.Size(107, 52);
+            this.Result_btn.TabIndex = 1;
+            this.Result_btn.Text = "Результат";
+            this.Result_btn.UseVisualStyleBackColor = false;
+            this.Result_btn.Click += new System.EventHandler(this.Result_btn_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "N";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 75;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Zn-Zточн";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 175;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Zn-Zn(*1)";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 175;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Zn(*1)-Zточн";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 175;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Deltan";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 175;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Zn(*2)-Zточн";
+            this.Column6.MinimumWidth = 6;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 175;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Deltan(1)";
+            this.Column7.MinimumWidth = 6;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 175;
+            // 
+            // Main_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1209, 561);
+            this.Controls.Add(this.Result_btn);
+            this.Controls.Add(this.dataGridView);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторная работа №4";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button Result_btn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Method_of_calculation_4
+{
+    public partial class Main_Form : Form
+    {
+        double accurateZ = 10.5844484649508098;
+        double alpha = 1.1;
+        public Main_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Result_btn_Click(object sender, EventArgs e)
+        {
+            int n = 2;
+            double[] sums = new double[17];
+            double[] third = new double[17];
+            double[] cetvert = new double[17];
+            double[] deltan = new double[17];
+            double[] schestoy = new double[17];
+            double[] deltan1 = new double[17];
+            int iter = 0;
+            while (n <= 131072)
+            {
+                sums[iter] = Summa(n) - accurateZ;
+                third[iter] = Summa(n) - extrapolate(Summa(n), Summa(n / 2), (alpha - 1));
+                cetvert[iter] = extrapolate(Summa(n), Summa(n / 2), (alpha - 1)) - accurateZ;
+                deltan[iter] = cetvert[iter] / sums[iter];
+                if (n == 2)
+                {
+                    dataGridView.Rows.Add(n, sums[iter], third[iter], cetvert[iter], deltan[iter], "-", "-");
+                }
+                else
+                {
+                    double val = extrapolate(Summa(n), Summa(n / 2), (alpha - 1));
+                    double val2 = extrapolate(Summa(n / 2), Summa(n / 4), (alpha - 1));
+                    schestoy[iter] = extrapolate(val, val2, (alpha)) - accurateZ;
+                    deltan1[iter] = schestoy[iter] / cetvert[iter];
+                    dataGridView.Rows.Add(n, sums[iter], third[iter], cetvert[iter], deltan[iter], schestoy[iter], deltan1[iter]);
+                }
+                n *= 2;
+            }
+        }
+        double Summa(int n)
+        {
+            double sum = 0;
+            for (int i = 1; i <= n; i++)
+                sum += (1 / Math.Pow(i, alpha));
+            return sum;
+        }
+        double extrapolate(double zn, double znq, double k)
+        {
+            return (zn + ((zn - znq) / (Math.Pow(2, k) - 1)));
+        }
+
+    }
+}
